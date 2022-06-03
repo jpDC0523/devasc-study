1ed38ff618aa09582b8b6ed46b28cfc47cc6344c
jpDC0523 <shinchibukai1023@gmail.com>
2022-06-03 23:31:59 +0800

    Parsing XML using the ElementTree Library

---
+++ parsexml.py
@@ -0,0 +1,19 @@
+# Date: 06/03/2022
+# Parsing XML using python!
+
+import xml.etree.ElementTree as ET
+
+stream = open('sample.xml', 'r')
+
+xml = ET.parse(stream)
+
+root = xml.getroot()
+
+for person in root.findall('Person'):
+    person_id = person.get('Id')
+    first_name = person.find('FirstName').text
+    last_name = person.find('LastName').text
+    email_addr = person.find('Email').text
+    print(f"Full Name: {first_name} {last_name}")
+    print(f"Person's ID Number: {person_id}")
+    print(f"E-mail Address: {email_addr}\n")
